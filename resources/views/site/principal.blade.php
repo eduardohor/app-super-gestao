77eacf4d1df0705249c0f9bfbcee399117c0cf3c
Eduardo Henrique <eduardo.hor@outlook.com>
2022-11-24 10:48:47 -0300

    add routes

--- resources/views/site/principal.blade.php
+++ resources/views/site/principal.blade.php
@@ -1,1 +1,7 @@
 <h1>Princial (view)</h1>
+
+<ul>
+    <li><a href="/">Principal</a></li>
+    <li><a href="/sobre-nos">Sobre Nós</a></li>
+    <li><a href="contato">Contato</a></li>
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,9 @@ Route::get('/', 'PrincipalController@principal');
 Route::get('/sobre-nos', 'SobreNosController@sobreNos');
 
 Route::get('/contato', 'ContatoController@contatos');
+
+Route::get('/login', function(){return 'Login';});
+Route::get('/clientes', function(){return 'Clientes';});
+Route::get('/fornecedores', function(){return 'Fornecedores';});
+Route::get('/produtos', function(){return 'Podutos';});
+
