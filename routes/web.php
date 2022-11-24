d7b967835f776a843277129d09d284c7979acb83
Eduardo Henrique <eduardo.hor@outlook.com>
2022-11-24 11:44:16 -0300

    add follback

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', 'PrincipalController@principal');
+Route::get('/', 'PrincipalController@principal')->name('site.index');
+Route::get('/sobre-nos', 'SobreNosController@sobreNos')->name('site.sobrenos');
+Route::get('/contato', 'ContatoController@contatos')->name('site.contato');
+Route::get('/login', function(){return 'Login';})->name('site.login');
 
-Route::get('/sobre-nos', 'SobreNosController@sobreNos');
+Route::prefix('/app')->group(function(){
+  Route::get('/clientes', function(){return 'Clientes';})->name('app.clientes');
+  Route::get('/fornecedores', function(){return 'Fornecedores';})->name('app.fornecedores');
+  Route::get('/produtos', function(){return 'Podutos';})->name('app.produtos');
 
-Route::get('/contato', 'ContatoController@contatos');
+});
 
-Route::get('/login', function(){return 'Login';});
-Route::get('/clientes', function(){return 'Clientes';});
-Route::get('/fornecedores', function(){return 'Fornecedores';});
-Route::get('/produtos', function(){return 'Podutos';});
+Route::fallback(function(){
+  echo 'A rota acessada não existe, <a href="'.route('site.index').'"> clique aqui </a>para ir para página inicial';
+});
 
