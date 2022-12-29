01a78fe27a1ecdc066815f37bd19f4f809721b3c
Eduardo Henrique <eduardo.hor@outlook.com>
2022-12-29 16:21:31 -0300

    add migrate unidades

--- database/migrations/2022_12_29_183835_create_produto_detalhes_table.php
+++ database/migrations/2022_12_29_183835_create_produto_detalhes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateProdutosDetalhesTable extends Migration
+class CreateProdutoDetalhesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateProdutosDetalhesTable extends Migration
      */
     public function up()
     {
-        Schema::create('produtos_detalhes', function (Blueprint $table) {
+        Schema::create('produto_detalhes', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('produto_id');
             $table->float('comprimento', 8,2);
@@ -33,6 +33,6 @@ class CreateProdutosDetalhesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('produtos_detalhes');
+        Schema::dropIfExists('produto_detalhes');
     }
 }
