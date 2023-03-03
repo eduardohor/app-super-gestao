43c0d57b3b4f74b7a9557aaa2960775df8400e40
Eduardo Henrique <eduardo.hor@outlook.com>
2023-03-03 15:05:47 -0300

    feat:add save contatos

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -2,14 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contatos(){
+	public function contatos()
+	{
+		return view('site.contato');
+	}
 
-        var_dump($_POST);
-        return view('site.contato');
+	public function salvar(Request $request)
+	{
+		$request->validate([
+			'nome' => 'required|min:3|max:40',
+			'telefone' => 'required',
+			'email' => 'required',
+			'motivo_contato' => 'required',
+			'mensagem' => 'required|max:2000'
+		]);
 
-    }
+		SiteContato::create($request->all());
+	}
 }
