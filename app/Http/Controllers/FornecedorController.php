bdf2e6c8425bb6d885c0cdc2696f0e829c3318df
Eduardo Henrique <eduardo.hor@outlook.com>
2023-03-07 11:36:02 -0300

    feat:adicionando cadastro de fornecedores

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Fornecedor;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
@@ -15,8 +16,38 @@ class FornecedorController extends Controller
 			return view('app.fornecedor.listar');
 		}
 
-		public function adicionar ()
+		public function adicionar (Request $request)
 		{
-			return view('app.fornecedor.adicionar');
+
+			$msg = '';
+
+			if ($request->_token) {
+				$regras = [
+					'nome' => 'required|min:3|max:40',
+					'site' => 'required', 
+					'uf' => 'required|min:2|max:2', 
+					'email' => 'email', 
+				];
+
+				$feedback = [
+					'required' => 'O campo :attribute deve ser preenchido',
+					'nome.min' => 'O campo nome deve ter no mínimo 3 caracteres',
+					'nome.max' => 'O campo nome deve ter no máximo 40 caracteres',
+					'uf.min' => 'O campo nome deve ter no mínimo 2 caracteres',
+					'nome.max' => 'O campo nome deve ter no máximo 2 caracteres',
+					'email' => 'O campo e-mail não foi preenchido corretamente'
+				];
+
+				$request->validate($regras, $feedback);
+
+				$fornecedor = new Fornecedor();
+
+				$fornecedor->create($request->all());
+
+				$msg = 'Cadastro Realizado com sucesso!';
+
+			}
+
+			return view('app.fornecedor.adicionar', ['msg' => $msg]);
 		}
 }
--- resources/views/app/fornecedor/adicionar.blade.php
+++ resources/views/app/fornecedor/adicionar.blade.php
@@ -17,12 +17,26 @@
 	</div>
 
 	<div class="informacao-pagina">
+		{{ $msg }}
 		<div style="width: 30%; margin-left: auto; margin-right: auto;">
-			<form action="" method="post">
-				<input type="text" name="nome" placeholder="Nome" class="borda-preta">
-				<input type="text" name="site" placeholder="Site" class="borda-preta">
-				<input type="text" name="uf" placeholder="UF" class="borda-preta">
-				<input type="text" name="email" placeholder="E-mail" class="borda-preta">
+			<form action="{{ route('app.fornecedor.adicionar') }}" method="post">
+				@csrf
+				<input type="text" name="nome" placeholder="Nome" class="borda-preta" value="{{ old('nome') }}">
+				@error('nome')
+						{{ $message }}
+				@enderror
+				<input type="text" name="site" placeholder="Site" class="borda-preta" value="{{ old('site') }}">
+				@error('site')
+						{{ $message }}
+				@enderror
+				<input type="text" name="uf" placeholder="UF" class="borda-preta" value="{{ old('uf') }}">
+				@error('uf')
+						{{ $message }}
+				@enderror
+				<input type="text" name="email" placeholder="E-mail" class="borda-preta" value="{{ old('email') }}">
+				@error('email')
+						{{ $message }}
+				@enderror
 				<button type="submit" class="borda-preta">Cadastrar</button>
 			</form>
 		</div>
